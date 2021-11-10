bc222beb3dcf11c972fa3b7f884061e27a2b615c
Ali EP <p30arena@gmail.com>
2021-11-10 08:34:06 +0330

    efficientnet is too heavy ~5MB binary and ~30MB code

---
+++ quality-control/test/pre-model/eval.py
@@ -0,0 +1,78 @@
+# https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/vision/ipynb/image_classification_efficientnet_fine_tuning.ipynb
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tensorflow as tf
+import pathlib
+
+PATH = os.path.join('out', 'cats_and_dogs_filtered')
+model_path = pathlib.Path('out/model')
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+
+BATCH_SIZE = 32
+IMG_SIZE = (96, 96)
+
+train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
+                                                            shuffle=True,
+                                                            batch_size=BATCH_SIZE,
+                                                            image_size=IMG_SIZE)
+validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                                 shuffle=True,
+                                                                 batch_size=BATCH_SIZE,
+                                                                 image_size=IMG_SIZE)
+class_names = train_dataset.class_names
+
+val_batches = tf.data.experimental.cardinality(validation_dataset)
+test_dataset = validation_dataset.take(val_batches // 5)
+validation_dataset = validation_dataset.skip(val_batches // 5)
+
+print('Number of validation batches: %d' %
+      tf.data.experimental.cardinality(validation_dataset))
+print('Number of test batches: %d' %
+      tf.data.experimental.cardinality(test_dataset))
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip('horizontal'),
+    tf.keras.layers.RandomRotation(0.2),
+])
+
+# efficientnet expects floating [0, 255]
+# preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+# rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1) # scales [0, 255] to [-1, 1]
+
+# Create the base model from the pre-trained model MobileNet V2
+IMG_SHAPE = IMG_SIZE + (3,)
+if model_path.exists():
+    model = tf.keras.models.load_model(model_path)
+else:
+    print("you must first run trian.py")
+    exit(1)
+
+# Retrieve a batch of images from the test set
+image_batch, label_batch = test_dataset.as_numpy_iterator().next()
+predictions = model.predict_on_batch(image_batch).flatten()
+
+# Apply a sigmoid since our model returns logits
+predictions = tf.nn.sigmoid(predictions)
+predictions = tf.where(predictions < 0.5, 0, 1)
+
+print('Predictions:\n', predictions.numpy())
+print('Labels:\n', label_batch)
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image_batch[i].astype("uint8"))
+    plt.title(class_names[predictions[i]])
+    plt.axis("off")
+
+plt.show()
